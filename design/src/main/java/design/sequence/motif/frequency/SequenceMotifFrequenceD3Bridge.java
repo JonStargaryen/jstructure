e4a6c5d8d9f68b0ea02535d4961b58924d196387
S <bittrich@hs-mittweida.de>
2016-11-25 15:18:28 +0100

    d3 sequence motif frequency renderer

---
+++ design/src/main/java/design/sequence/motif/frequency/SequenceMotifFrequenceD3Bridge.java
@@ -0,0 +1,10 @@
+package design.sequence.motif.frequency;
+
+/**
+ * Created by S on 25.11.2016.
+ */
+public class SequenceMotifFrequenceD3Bridge {
+    public SequenceMotifFrequenceD3Bridge(SequenceMotifFrequencyCalculator sequenceMotifFrequencyCalculator) {
+
+    }
+}
