92ec8b9c089124a4d862dab830cfe06ed5eced5d
S <bittrich@hs-mittweida.de>
2017-09-11 08:15:02 +0200

    clean-up

---
+++ jstructure-membrane/src/main/java/de/bioforscher/jstructure/membrane/MembraneConstants.java
@@ -0,0 +1,70 @@
+package de.bioforscher.jstructure.membrane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * Shared class of project constants and convenience functions used in the membrane modules.
+ */
+public class MembraneConstants {
+    private static final Logger logger = LoggerFactory.getLogger(MembraneConstants.class);
+    public static final Path GIT_DIRECTORY = Paths.get(System.getProperty("user.home")).resolve("git");
+    public static final Path PHD_DIRECTORY = GIT_DIRECTORY.resolve("phd_sb_repo");
+    public static final Path DATA_DIRECTORY = PHD_DIRECTORY.resolve("data");
+    public static final Path DATASETS_DIRECTORY = DATA_DIRECTORY.resolve("datasets");
+    public static final Path PDBTM_ALPHA_DATASET_DIRECTORY = DATASETS_DIRECTORY.resolve("pdbtm_alpha");
+    public static final Path PDBTM_ALPHA_DATASET_PDB_DIRECTORY = PDBTM_ALPHA_DATASET_DIRECTORY.resolve("pdb");
+    public static final Path PDBTM_BETA_DATASET_DIRECTORY = DATASETS_DIRECTORY.resolve("pdbtm_beta");
+    public static final Path PDBTM_BETA_DATASET_PDB_DIRECTORY = PDBTM_BETA_DATASET_DIRECTORY.resolve("pdb");
+
+    public static Stream<String> lines(Path path) {
+        try {
+            return Files.lines(path);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static Stream<String> lines(String url) {
+        try {
+            return new BufferedReader(new InputStreamReader(new URL(url).openStream())).lines();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static void write(Path path, String content) {
+        ensureDirectoriesExist(path);
+        try {
+            Files.write(path, content.getBytes());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void ensureDirectoriesExist(Path path) {
+        if(Files.isDirectory(path)) {
+            throw new IllegalArgumentException(path.toFile().getAbsolutePath() + " is no regular file - cannot process");
+        }
+
+        Path parentDirectory = path.getParent();
+        if(!Files.exists(parentDirectory)) {
+            logger.info("creating directory '{}'", parentDirectory.toFile().getAbsolutePath());
+            try {
+                Files.createDirectories(parentDirectory);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+}
---
+++ jstructure-membrane/src/main/java/de/bioforscher/jstructure/membrane/T01_DownloadPdbtmAlphaNrDataset.java
@@ -0,0 +1,48 @@
+package de.bioforscher.jstructure.membrane;
+
+import de.bioforscher.jstructure.model.structure.Chain;
+import de.bioforscher.jstructure.model.structure.Structure;
+import de.bioforscher.jstructure.model.structure.StructureParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Query the PDBTM and obtain the current list of non-redundant alpha-helical chain ids. Ensure that the corresponding
+ * chain is present in the PDB, otherwise the chain is ignored and will not be present in the local list.
+ */
+public class T01_DownloadPdbtmAlphaNrDataset {
+    private static final Logger logger = LoggerFactory.getLogger(T01_DownloadPdbtmAlphaNrDataset.class);
+    private static final String FETCH_URL = "http://pdbtm.enzim.hu/data/pdbtm_alpha_nr.list";
+
+    public static void main(String[] args) {
+        String selectedIds = MembraneConstants.lines(FETCH_URL)
+                .filter(T01_DownloadPdbtmAlphaNrDataset::handlePdbId)
+                .collect(Collectors.joining(System.lineSeparator()));
+        MembraneConstants.write(MembraneConstants.PDBTM_ALPHA_DATASET_DIRECTORY.resolve("pdbtm_alpha_nr.list"), selectedIds);
+    }
+
+    private static boolean handlePdbId(String chainId) {
+        try {
+            String pdbId = chainId.split("_")[0];
+            String chainIdentifier = chainId.split("_")[1];
+            Structure structure = StructureParser.source(pdbId).minimalParsing(true).parse();
+            Optional<Chain> chain = structure.select()
+                    .chainId(chainIdentifier)
+                    .asOptionalChain();
+            if(!chain.isPresent()) {
+                logger.warn("{} is missing in structure", chainId);
+                return false;
+            }
+            MembraneConstants.write(MembraneConstants.PDBTM_ALPHA_DATASET_PDB_DIRECTORY.resolve(pdbId + ".pdb"),
+                    structure.getPdbRepresentation());
+            logger.info("{} is present", chainId);
+            return true;
+        } catch (Exception e) {
+            logger.warn("{} is obsolete", chainId);
+            return false;
+        }
+    }
+}
