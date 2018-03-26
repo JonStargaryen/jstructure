368b2df43ac96c6e88ef7939a2d7830fbac8a5cb
S <bittrich@hs-mittweida.de>
2018-03-26 21:26:24 +0200

    integration of structural information results

--- jstructure-structural-information/src/test/java/de/bioforscher/jstructure/si/visualization/StructuralInformationParserServiceTest.java
+++ jstructure-structural-information/src/test/java/de/bioforscher/jstructure/si/visualization/StructuralInformationParserServiceTest.java
@@ -5,34 +5,44 @@ import de.bioforscher.jstructure.efr.parser.Start2FoldXmlParser;
 import de.bioforscher.jstructure.model.structure.Chain;
 import de.bioforscher.jstructure.model.structure.StructureParser;
 import de.bioforscher.jstructure.model.structure.aminoacid.AminoAcid;
-import org.junit.Ignore;
+import de.bioforscher.testutil.TestUtils;
 import org.junit.Test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StructuralInformationParserServiceTest {
     @Test
-    @Ignore
     public void shouldParseStructuralInformationFile() {
         Chain chain = StructureParser.fromPdbId("1bdd")
                 .parse()
                 .getFirstChain();
+        List<AminoAcid> aminoAcids = chain.aminoAcids().collect(Collectors.toList());
 
         Start2FoldXmlParser.parseSpecificExperiment(chain,
-                Paths.get("/home/bittrich/git/phd_sb_repo/data/start2fold/xml/STF0045.xml"),
+                TestUtils.getResourceAsInputStream("efr/STF0045.xml"),
                 Stream.of(185).collect(Collectors.toList()));
 
         List<AminoAcid> earlyFoldingResidues = chain.aminoAcids()
                 .filter(aminoAcid -> aminoAcid.getFeature(Start2FoldResidueAnnotation.class).isEarly())
                 .collect(Collectors.toList());
 
-        Path path = Paths.get("/home/bittrich/git/phd_sb_repo/data/si/raw/STF0045.out");
-        StructuralInformationParserService.getInstance().parseStructuralInformationFile(path,
-                earlyFoldingResidues)
+        List<ContactStructuralInformation> contactStructuralInformation = StructuralInformationParserService.getInstance()
+                .parseContactStructuralInformationFile(TestUtils.getResourceAsInputStream("si/STF0045.out"),
+                        earlyFoldingResidues);
+
+        contactStructuralInformation.stream()
+                .map(ContactStructuralInformation::getCsvLine)
+                .forEach(System.out::println);
+
+        System.out.println();
+        List<ResidueStructuralInformation> residueStructuralInformation = StructuralInformationParserService.getInstance()
+                .composeResidueStructuralInformation(aminoAcids,
+                        earlyFoldingResidues,
+                        contactStructuralInformation);
+        residueStructuralInformation.stream()
+                .map(ResidueStructuralInformation::getCsvLine)
                 .forEach(System.out::println);
     }
 }
